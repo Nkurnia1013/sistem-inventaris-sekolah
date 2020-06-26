74bb633c6722aafde2f64259e4697a097d08972f
Nkurnia1013 <nkurnia1031@gmail.com>
2020-06-26 17:33:54 +0700

    Initial commit

---
+++ views/Komponen/Kop.php
@@ -0,0 +1,57 @@
+<div style="border-bottom: solid;">
+    <div class="d-flex justify-content-start tes2 mt-3">
+        <div class="mr-5">
+            <img src="mine/logo1.jpg" width="200" alt="Logo">
+        </div>
+        <div class="mx-auto align-self-center text-center">
+            <p class="m-0 p-0">
+                <b class="text-success" style="font-size: 20pt;">
+                    YAYASAN FII SABILILLAH
+                </b>
+            </p>
+            <p class="m-0 p-0">
+                <b class="text-dark" style="font-size: 20pt;">
+                    SEKOLAH MENENGAH PERTAMA
+                </b>
+            </p>
+            <p class="m-0 p-0">
+                <b class="text-dark" style="font-size: 20pt;">
+                    (SMP) FII SABILILLAH
+                </b>
+            </p>
+            <b>
+            <p class="m-0 p-0">
+                <span class="text-dark" style="font-size: 12pt;">
+                    SK KEMENKUMHAM NO: AHU-0043020. AH. 01.04. TH. 2016
+                </span>
+            </p>
+
+            <p class="m-0 p-0">
+                <span class="text-dark" style="font-size: 12pt;">
+                    Alamat : Jl. Dr. Wahidin No. 26 Kel. Bintan Kec. Dumai Kota
+                </span>
+            </p>
+            <p class="m-0 p-0">
+                <span class="text-dark" style="font-size: 12pt;">
+                    Kode Pos: 28812 Hp: 082285260622
+                </span>
+            </p>
+            </b>
+            <br>
+        </div>
+         <div class="mr-5">
+            <img src="mine/logo2.png" style="height: 200px" width="200" alt="Logo">
+        </div>
+    </div>
+    <div class="mx-5 px-5 justify-content-between d-flex text-justify">
+
+        <span class="text-dark mr-auto" style="font-size: 12pt;">
+           <b> NSS: 204090205043</b>
+        </span>
+        <span class="text-dark ml-auto" style="font-size: 12pt;">
+           <b>  NDS: 202090610050</b>
+        </span>
+
+
+    </div>
+</div>
